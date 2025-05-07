5c121cf6d98994919ebf19f5835d926371669684
nsavilaa <nsavila@fi.uba.ar>
2025-05-07 16:08:26 -0300

    Clase Vacio que hereda de Pieza

---
+++ Vacio.java
@@ -0,0 +1,30 @@
+/**
+ * Clase que representa un sector vacío en el tablero
+ * Hereda de la Pieza y se utiliza para representar espacios
+ * donde no hay naves, bases, satélites ni radiación
+*/
+public class Vacio extends Pieza {
+    private static final String NOMBRE_POR_DEFECTO = "_";
+    private static final int VIDA_POR_DEFECTO = 0;
+
+    /**
+     * Construye un sector Vacio en las coordenadas dadas
+     * @param x Coordenada X
+     * @param y Coordenada Y
+     * @param z Coordenada Z
+     */
+    public Vacio(int x, int y, int z) {
+        super(TipoPieza.VACIO, null, x,y,z, NOMBRE_POR_DEFECTO, VIDA_POR_DEFECTO);
+    }
+
+    /**
+     * Mátodo para verificar si un sector está vacío
+     * @return true, ya que representa si un sector esta vacío
+    */
+    public boolean estaVacio() {
+        return true;
+    }
+
+ 
+
+}
